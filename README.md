68eb46b041502ae7c4b65367acedfdfd4f3fae3b
JKAmbe <89562604+JKAmbe@users.noreply.github.com>
2022-09-21 11:33:19 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FunnelAssessment
+
+Developed with Unreal Engine 4
---
+++ Source/FunnelAssessment/FunnelAssessmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FunnelAssessmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUNNELASSESSMENT_API AFunnelAssessmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
