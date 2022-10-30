8a1b5ebbc713a552631c6829a6c8cd422c9221a3
yuzusiooooo <um60ac20@gmail.com>
2022-10-31 05:00:51 +1100

    player health

---
+++ Source/FunnelAssessment/HealthComponent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class FUNNELASSESSMENT_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void TakeDmage(float Damage);
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	UPROPERTY(EditAnywhere)
+		float MaxHealth = 100.0f;
+	UPROPERTY(Replicated)
+		float CurrentHealth;
+};
